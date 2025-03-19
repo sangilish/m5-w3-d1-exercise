36fbbb6b140ff09f010e75d69cbb8b53d22ccb34
sangilish <56685007+sangilish@users.noreply.github.com>
2025-03-18 19:46:44 -0700

    m5-w3-d1-exercise

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+
+
 import OAuth2Login from './OAuth2Login';
 
 export default OAuth2Login;
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";  // 이 줄이 있는지 확인!
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("example") // index.html에 "example" ID가 존재하는지도 확인
+);
